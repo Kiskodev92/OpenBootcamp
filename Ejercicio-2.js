d12e68333e0c61990bae8f4c222c9e3b9e411a98
Kiskotkd <116261484+Kiskotkd@users.noreply.github.com>
2022-11-08 12:34:41 +0100

    Ejercicio listas JS

---
+++ Ejercicio-2.js
@@ -0,0 +1,15 @@
+const lista= [30, "Francisco García", true,]
+
+console.log(lista)
+
+const fecha_nacimiento= new Date("November 02 1992");
+console.log(fecha_nacimiento)
+
+const libro= {
+    Titulo:"Libro de Cocina",
+    Autor:"Eduardo Lopez",
+    Publicacion:"20/04/2010",
+    Direccion: "Amazon",
+}
+console.log (libro.Autor)
+
