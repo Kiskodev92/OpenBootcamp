e3f7f5e3cfdcb83eb58a5f2d771a898adada0da5
Kiskotkd <116261484+Kiskotkd@users.noreply.github.com>
2022-11-10 11:26:34 +0100

    Ejercicio-cadenas-JS

---
+++ Ejercicio-cadenas.js
@@ -0,0 +1,17 @@
+let nombre="Francisco";
+let apellido="García";
+let estudiante="Hola, soy el estudiante"
+// cadena de estudiante
+console.log(estudiante.concat(" ", nombre," ",apellido))
+//cadena minus
+console.log(estudiante.toLowerCase())
+//cadena mayus
+console.log(estudiante.toUpperCase())
+//cadena contando todos los espacios de estudiante.
+console.log(estudiante.length)
+//primera letra de Nombre
+console.log(nombre.charAt(0))
+//ultima letra de apullido
+console.log(apellido.lastIndexOf("García"))
+
+Boolean 
