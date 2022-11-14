9b071c60272bd1f2ddbf5f5dc99c52a3205d1cf5
Kiskotkd <116261484+Kiskotkd@users.noreply.github.com>
2022-11-14 11:28:15 +0100

    Ejercicio Numbers JS

---
+++ Ejercicio-Numbers.js
@@ -0,0 +1,7 @@
+const altura_cm = 182;
+const altura_metros= 1.82
+const peso = 86.5
+const max_altura = (altura_metros.max_precision);
+const min_peso = (peso.min_precision);
+
+const valor_maximo= Number.EPSILON +1;
