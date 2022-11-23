91fecf61a404772caadc11ccace4256b6401c988
Kiskotkd <116261484+Kiskotkd@users.noreply.github.com>
2022-11-23 09:37:33 +0100

    Ejercicio 3 sets

---
+++ Ejercicio-3.js
@@ -0,0 +1,11 @@
+const fechahoy= new Date ()
+console.log(fechahoy)
+
+const nacimiento = new Date (1992,10,2)
+console.log(nacimiento)
+
+console.log(fechahoy > nacimiento)
+
+console.log(nacimiento.getMonth()+1)
+
+console.log(nacimiento.getFullYear())
