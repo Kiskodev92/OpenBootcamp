5f061d70d787441fa6bcb3fa5c7b9172075a93d3
Kiskotkd <116261484+Kiskotkd@users.noreply.github.com>
2022-11-28 10:56:31 +0100

    Ejercicio error

---
+++ Ejercicio-error.js
@@ -0,0 +1,29 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+
+
+
+  try {
+    // Código que puede fallar
+    console.log("Esta ejecutandose de manera correcta")
+    const doble= miFuncion(numero)
+    console.log(doble)
+} catch(e){
+    //En caso de fallar, quiero que ejecutes
+    console.error(`El valor de e es: ${e}`)
+    console.error("ERROR!")
+} finally {
+    console.log("Esto se va a ejecutar tanto si se produce algun error como si no existe ninguno")
+}
