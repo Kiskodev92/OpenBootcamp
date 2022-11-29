dfe11fcc9227e16302ae513064d52198729bded6
Kiskotkd <116261484+Kiskotkd@users.noreply.github.com>
2022-11-29 12:17:48 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+function suma (a, b) {
+    return a+b
+}
+const sum= Suma(1,2)
+console.log(sum)
+function multi (a,b) {
+    return a*b
+}
+const multi= multi(4,5)
+console.log(multi)
+
+import chalk from 'chalk';
+
+const log = console.log;
+
+
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+
+log(chalk.blue.bgRed.bold('Hello world!'));
+
+
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+
+
+log(chalk.green(
+	'I am a green line ' +
+	chalk.blue.underline.bold('with a blue substring') +
+	' that becomes green again!'
+));
