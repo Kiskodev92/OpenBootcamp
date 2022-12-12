7c61839fbb5c1291bffa03b3e00df2f12e6f89a7
Kiskotkd <116261484+Kiskotkd@users.noreply.github.com>
2022-12-12 12:10:45 +0100

    Add files via upload

---
+++ ejercicio-depuracion.js
@@ -0,0 +1,9 @@
+function obtenNumero(num){
+    let arrayNums=[]
+    for (let i=0; i<10; i++){
+        const numero = persona.edad + i
+        console.log(numero)
+        arrayNums=[...arrayNums, numero]
+    }
+  return arrayNums
+}
